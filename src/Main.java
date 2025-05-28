f360c52a602156c907019fa975d0c9e03395c577
stanley <malosestanleyaa@gmail.com>
2025-05-28 20:05:16 +0200

    create the fibonaci code

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//this will output first 12 fibonacci sequence numbers
+// 0 1 1 2 3 5 8 13 21 34 55 89 144
+
+public class Main {
+    public static void main(String[] args) {
+
+        int first = 0;
+        int last = 1;
+
+        System.out.print(first + " ");
+
+        //so i put 11 because it will be minus the first sys out
+        for (int i = 1; i <= 11; i++) {
+            first = first + last;
+
+            System.out.print(first + " ");
+
+            last = first - last;
+        }
+    }
+}
